82a3a55ae376a41baf238709379c825ca33b1109
Gustavo <gustavo.larcoj@gmail.com>
2025-02-01 16:41:58 -0600

    siembra de datos

--- BlogCore/obj/Debug/net8.0/BlogCore.AssemblyInfo.cs
+++ BlogCore/obj/Debug/net8.0/BlogCore.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlogCore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dfbaeb4f54dba8752e8db4e80fbf03ead852b1fb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8e9b9b96ff086fc01c7c22fa7320307ede54f68")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlogCore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlogCore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
